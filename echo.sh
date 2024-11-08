569473b50086d0edee0b6552c5fdcb2df65e2218
Alexander Legeyda <alex@legeyda.com>
2024-11-08 21:45:14 +0300

    refactor

--- echo.sh
+++ echo.sh
@@ -1,3 +1,12 @@
-bobshell_echo() {
-	printf %s "$*"
+
+
+# test library for debug
+
+
+shelduck "${SHELDUCK_BASE_URL}/string.sh"
+
+shelduck_echo() {
+	shelduck_echo_args="$*"
+	printf %s "$shelduck_echo_args" " ($(bobshell_starts_with "$shelduck_echo_args" a && echo starts with a || echo does not start with a))"
+	unset shelduck_echo_args
 }

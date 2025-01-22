e06547a3d209ab96ab07c15619c2f65d1a68be7a
Alexander Legeyda <alex@legeyda.com>
2025-01-22 22:35:04 +0300

    update bobshell

--- shelduck.sh
+++ shelduck.sh
@@ -1466,7 +1466,9 @@ bobshell_locator_is_stdout() {
 # fun: bobshell_is_file LOCATOR [FILEPATHVAR]
 bobshell_locator_is_file() {
 	if bobshell_starts_with "$1" /; then
-		printf %s "$1"
+		if [ -n "${2:-}" ]; then
+			bobshell_putvar "$2" "$1"
+		fi
 	else
 		bobshell_remove_prefix "$1" file: "${2:-}"
 	fi
